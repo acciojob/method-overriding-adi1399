a1ff69a139155593ed21d489a199b012925fd8ac
adi1399 <120732295+adi1399@users.noreply.github.com>
2023-01-11 19:48:21 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
+import java.util.*;
+
+ class A{
+  public void meth(){
+    System.out.println("Invoking method from class A");
+  }
+}
+
+ class B extends A{
+  B(){
+    super.meth();
+  }
+  public void meth(){
+    System.out.println("Method is overridden in Extendend class B");
+  }
+}
 
 public class Main {
-  
+	public static void main(String[] args){
+  B obj=new B();
+		obj.meth();
+	}
 }
